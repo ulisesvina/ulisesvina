96f23230566d63ae811bccd75ec148ce5ec04729
Ulises Viña <contacto@ulisesvina.me>
2021-07-04 21:53:44 -0500

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,13 +4,13 @@ const Intro = () => {
             <h3>Contacto</h3>
             <div className="mt-5">
                 <h5><span className="icon-instagram"></span> Instagram</h5>
-                <a className="link" target="_BLANK" href="https://instagram.com/ulisesvina">@ulisesvina</a>
+                <a className="link" rel="noreferrer" target="_BLANK" href="https://instagram.com/ulisesvina">@ulisesvina</a>
                 <h5 className="mt-4"><span className="icon-twitter mr-2"></span> Twitter</h5>
-                <a className="link" target="_BLANK" href="https://twitter.com/ulisesvina">@ulisesvina</a>
+                <a className="link" rel="noreferrer" target="_BLANK" href="https://twitter.com/ulisesvina">@ulisesvina</a>
                 <h5 className="mt-4"><span className="icon-github mr-2"></span> GitHub</h5>
-                <a className="link" target="_BLANK" href="https://github.com/ulisesvina">@ulisesvina</a>
+                <a className="link" rel="noreferrer" target="_BLANK" href="https://github.com/ulisesvina">@ulisesvina</a>
                 <h5 className="mt-4"><span className="icon-youtube mr-2"></span> YouTube</h5>
-                <a className="link" target="_BLANK" href="https://www.youtube.com/c/UlisesViña">Ulises Viña</a>
+                <a className="link" rel="noreferrer" target="_BLANK" href="https://www.youtube.com/c/UlisesViña">Ulises Viña</a>
             </div>
         </div>
     )
