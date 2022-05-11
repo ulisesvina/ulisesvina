bef55962a7ff4b540cf8d57766dc3f6fc198237e
Ulises Viña <ulisesvina@MacBook-Pro-de-Ulises.local>
2022-05-11 13:55:15 -0500

    Added a few things in the index page

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,12 @@ const fetchNowPlaying = setData => {
 }
 
 const Home = () => {
-  const [data, setData] = useState({ isPlaying: false }); let fetched = false;
+  const [data, setData] = useState({ isPlaying: false }); let fetched = false,
+  [state, setState] = useState({
+    quote: 1,
+    char: 0
+  }),
+  quotes = ["innovative", "useful", "incredible", "inspiring"];
 
   useEffect(() => {
     if (!fetched) {
@@ -23,11 +28,31 @@ const Home = () => {
     return () => clearInterval(interval);
   }, []);
 
+  useEffect(() => {
+    setTimeout(async () => {
+      if(state.char == quotes[state.quote].length) {
+          await new Promise(r => setTimeout(r, 1500));
+          setState({
+              quote: state.quote == 3 ? 0 : state.quote + 1,
+              char: 1,
+          })
+      } else {
+          setState({
+              quote: state.quote,
+              char: state.char + 1
+          })
+      }
+    }, 150);
+  });
+
   return (
     <div>
-      <h2>Home</h2>
+      <div className="center">
+        <h2>I design <b className={styles["gradient-text"]}>{quotes[state.quote].substring(0, state.char)}</b> products.</h2><br/>
+        <h3>Hi! I'm Ulises, an experienced fullstack developer focused on low-level.</h3>
+      </div>
       {
-        data["isPlaying"] ? <p><FaSpotify className={styles["spotify-icon"]}/> Now playing {data.songName} by {data.artist}</p> : <p><BiNoEntry className={styles["no-entry-icon"]}/> Currently not playing any song.</p>
+        data["isPlaying"] ? <p><FaSpotify className={`${styles["spotify-icon"]} icon`}/> Now playing <b>{data.songName}</b> by <b>{data.artist} ✨</b></p> : <p><BiNoEntry className={`${styles["no-entry-icon"]} icon`}/> Currently not playing any song.</p>
       }
     </div>
   );
