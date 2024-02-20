bed51324ff2424ff26c3a09b661e022f2ae0f698
Ulises Viña <contacto@ulisesvina.me>
2024-02-20 13:42:03 -0600

    Update About.js

--- components/Main/About.js
+++ components/Main/About.js
@@ -1,8 +1,20 @@
 import { useTranslation } from "react-i18next";
 
-const About = ({ age }) => {
+const About = () => {
+  const calcAge = () =>
+    ((new Date() - new Date("2006-08-09")) / 31557600000).toFixed(9);
+  
+  const [age, setAge] = useState(calcAge());
   const { t } = useTranslation();
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setAge(calcAge());
+    }, 100);
+
+    return () => clearInterval(interval);
+  }, []);
+  
   return (
     <div className="bg-secondary text-secondary-text p-4 px-6 rounded-xl flex flex-col justify-center">
       <h1 className="text-md uppercase font-bold">{t("about")}</h1>
