9cfc662fb5fbdbb0acf90e19f96bac0ab00ceaf2
Ulises Viña <contacto@ulisesvina.me>
2024-08-08 23:14:22 -0600

    Update About.js

--- components/Main/About.js
+++ components/Main/About.js
@@ -3,22 +3,19 @@ import { useEffect, useState } from "react";
 
 const About = () => {
   const calcAge = () => {
-      const today = new Date();
-      const birthDate = new Date("2006-08-09");
-      let age = today.getFullYear() - birthDate.getFullYear();
-      
-      const isBirthdayPassedThisYear = 
-          today.getMonth() > birthDate.getMonth() || 
-          (today.getMonth() === birthDate.getMonth() && today.getDate() >= birthDate.getDate());
-      
-      if (!isBirthdayPassedThisYear) {
-          age--;
-      }
+  const today = new Date();
+  const birthDate = new Date("2006-08-09");
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const monthDiff = today.getMonth() - birthDate.getMonth();
+    
+  if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
   
-      return age;
-  };
-  
-  const [age, setAge] = useState(calcAge());
+  return age;
+};
+
+const [age, setAge] = useState(calcAge());
   const { t } = useTranslation();
 
   useEffect(() => {
